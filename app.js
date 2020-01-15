bced2dd231001758d0797d95612ade87c36c5248
Wilson Espitia <wespitia@servinf.local>
2020-01-15 15:36:46 -0500

    Cargue base

---
+++ app.js
@@ -0,0 +1,6 @@
+import * as map from './src/js/map';
+import homeStyle from './src/css/style.css';
+
+map.hola();
+
+map.init();
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+	context: __dirname,
+  entry: './app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: path.join(__dirname, 'dist'),
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new MiniCssExtractPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader'
+        ],
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000
+  }
+};
